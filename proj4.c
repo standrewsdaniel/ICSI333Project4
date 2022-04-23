06cd3ba1c0155fcec4ec2332320dfab3e007b3e6
Daniel St Andrews <103073945+standrewsdaniel@users.noreply.github.com>
2022-04-23 10:08:50 -0400

    Add files via upload

---
+++ proj4.c
@@ -0,0 +1,33 @@
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~Project 4 Group Source Code File~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ICSI 333 Spring 2022 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Authors:
+	Daniel Najafi
+	Daniel St Andrews
+	Andrew Carv
+
+Purpose:
+	Use C language to write a simple web server using information about networking,
+threading, and file system calls.
+
+
+Changelog:
+-Created 4/23/22
+
+Methods:
+
+
+
+
+*/
+
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+}
