0636c2d416cf78d38f2ecfe2f9096ee54a2899dd
Daniel St Andrews <103073945+standrewsdaniel@users.noreply.github.com>
2022-04-30 14:59:14 -0400

    Add files via upload
    Added file verification

--- proj4.c
+++ proj4.c
@@ -19,13 +19,65 @@ Methods:
 
 
 
-
 */
 
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <pthread.h>
+#include<sys/dir.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
 
-int main(){
+
+#define PORT_NUM 8000
+#define SA struct sockaddr
+
+int fd;
+
+int main(int argc, char* argv[]) {
+
+	char* message1 = "HTTP/1.0";
+	char* message2 = "Content-Length:";
+	char* requestMgs = "GET";
+	int contLen = 0;
+	int success = 200;
+	int noFile = 404;
+	char* buf[BUFSIZ];
+
+	if (argc <= 1) {
+		printf("Not enough arguments! Exiting...\n");
+		fflush(stdout);
+		exit(1);
+	}
+	else {
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1) {
+			printf("%s", argv[1]);
+			printf("Error opening file! Exiting...");
+			fflush(stdout);
+			exit(1);
+		}
+		else{
+			read(fd, buf, BUFSIZ);
+			//This is where instead of writing to screen it can
+			// be stored and then sent to the server / client.
+			write(STDOUT_FILENO, buf, BUFSIZ);
+		}
+
+	}
 
 
 
